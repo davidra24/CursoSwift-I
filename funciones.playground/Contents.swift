960e9188ab5937d278e4ca54d1ac2961b593a246
David Ramirez <cdavidramirezg24@gmail.com>
2018-01-16 16:23:03 -0800

    funciones

--- funciones.playground/Contents.swift
+++ funciones.playground/Contents.swift
@@ -1,5 +1,26 @@
 //: Playground - noun: a place where people can play
+//Funciones
 
-import UIKit
+func funcionSinParametro(){
+    
+}
+func funcionImprimir(param0 : String){
+    print(param0)
+}
 
-var str = "Hello, playground"
+funcionImprimir(param0: "Hola mundo")
+
+var appleTv = 149.0
+var appleTvIva = appleTv * 0.15
+let appleTvPrecio = appleTv + appleTvIva
+
+funcionImprimir(param0: String(appleTvPrecio))
+
+func calcularIva(precio:Double) -> Double{
+    let iva = 0.15
+    let precioTotal = precio + (precio * iva)
+    return precioTotal
+}
+
+funcionImprimir(param0 : String(calcularIva(precio: 149.0)))
+funcionImprimir(param0 : String(calcularIva(precio: 258)))
